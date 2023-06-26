3e4f2acce7e3acb57f5067b7030c19878339bbfe
Kouji Sano <KoujiSano@users.noreply.github.com>
2023-06-26 05:49:34 +0000

    multientry

--- pages/home.vue
+++ pages/home.vue
@@ -5,7 +5,7 @@
         <div class="card">
           <div class="card-header">各種入力</div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item"><a href="#" class="card-link">複合仕訳入力</a></li>
+            <li class="list-group-item"><NuxtLink to="/multientry" class="card-link">複合仕訳入力</NuxtLink></li>
           </ul>
         </div>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="container">
-    <h1>Hello World</h1>
+    Hello World
   </div>
 </template>
