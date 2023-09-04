0a7f23a25d62e2b48baf1aa2fc5b58c0e60fc485
Kouji Sano <KoujiSano@users.noreply.github.com>
2023-09-04 03:21:22 +0000

    update nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,27 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     app: {
-        baseURL: '/Nuxt3Bootstrap5AppDemo/', // baseURL: '/<repository>/'
         head: {
             meta: [
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' }
             ],
             script: [
-                { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' }
+                {
+                    // src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' 
+                }
             ],
             link: [
-                { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' },
-                { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css' }
+                // { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' },
+                // { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css' }
             ]
         }
     },
     modules: [
         '@vueuse/nuxt',
-        '@nuxt/content'
-      ],
+        '@nuxt/content',
+        'usebootstrap'
+    ],
+    css: [
+        "bootstrap/scss/bootstrap.scss"
+    ]
 })
