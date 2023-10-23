f16a7197a5459ee06bbe0096ca5b542e58de23f8
sumna7 <104078088+syuu1230@users.noreply.github.com>
2023-10-23 14:57:36 +0900

    Create soukantei.vue

---
+++ pages/demo/Whiteshining_SeikisiSoukanteiMotocho_XxPROxX_Mrk2_mp4/soukantei.vue
@@ -0,0 +1,69 @@
+<template>
+  <Container>
+    <Row>
+      <Col col="6 md-3">
+        会計単位
+      </Col>
+      <Col col="6 md-3">
+        <Dropdown>
+        <DropdownToggle button="secondary">
+          本社
+        </DropdownToggle>
+        <DropdownMenu>
+        <DropdownItem>
+          本社
+        </DropdownItem>
+        </DropdownMenu>
+        </Dropdown>
+      </Col>
+      <Col col="6 md-3">
+        部門
+      </Col>
+      <Col col="6 md-3">
+        <Dropdown>
+          <DropdownToggle button="secondary">
+            指定なし
+          </DropdownToggle>
+          <DropdownMenu>
+          <DropdownItem>
+            指定なし
+          </DropdownItem>
+        </DropdownMenu>
+        </Dropdown>
+      </Col>
+      <Col>
+        勘定項目
+      </Col>
+      <Col>
+        <Dropdown>
+          <DropdownToggle button="secondary">
+            指定なし
+          </DropdownToggle>
+          <DropdownMenu>
+          <DropdownItem>
+            指定なし
+          </DropdownItem>
+        </DropdownMenu>
+        </Dropdown>
+      </Col>
+      <Col>
+        <a>
+          から
+        </a>
+      </Col>
+      <Col>
+        <Dropdown>
+          <DropdownToggle button="secondary">
+            指定なし
+          </DropdownToggle>
+          <DropdownMenu>
+          <DropdownItem>
+            指定なし
+          </DropdownItem>
+        </DropdownMenu>
+        </Dropdown>
+      </Col>
+    </Row>
+  </Container>
+
+</template>
