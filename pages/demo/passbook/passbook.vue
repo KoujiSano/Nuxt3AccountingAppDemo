54c87a0feb842f0133669b36d94776db745d69ee
sura8888 <surasena221108@gmail.com>
2023-11-06 14:54:01 +0900

    11/06

--- pages/demo/passbook/passbook.vue
+++ pages/demo/passbook/passbook.vue
@@ -1,5 +1,100 @@
 <template>
     <div class="container">
-
+      <div class="d-flex">
+        <div class="p-2 bg-light bg-gradient" style="width:6em;">年度</div>
+        <div class="p-2 flex-fill">2023</div>
+        <div class="p-2 bg-light bg-gradient" style="width:6em;">区分</div>
+        <div class="p-2 flex-fill">2023</div>
+      </div>
+      <div class="d-flex">
+        <div class="d-flex align-content-center flex-wrap border bg-light bg-gradient" style="width:2em;">
+          <div class="p-2">1</div>
+        </div>
+        <div class="flex-fill">
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+        </div>
+        <div class="flex-fill">
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
+    <div class="container">
+      <div class="d-flex bg-light bg-gradient">
+        <div class="d-flex align-content-center flex-wrap border" style="width:2em;">
+          <div class="p-2">行</div>
+        </div>
+        <div class="flex-fill ">
+          <div class="d-flex">
+            <div class="p-2 border w-25">日付</div>
+            <div class="p-2 flex-fill border">摘要</div>
+            <div class="p-2 flex-fill border">科目</div>
+            <div class="p-2 flex-fill border">支払い金額</div>
+          </div>
+          <div class="d-flex">
+            <div class="p-2 border w-25">預かり金額</div>
+            <div class="p-2 flex-fill border">差引残高</div>
+          </div>
+        </div>
+        <div class="flex-fill">
+            <div class="d-flex">
+            <div class="p-2 border w-25">日付</div>
+            <div class="p-2 flex-fill border">摘要</div>
+            <div class="p-2 flex-fill border">科目</div>
+            <div class="p-2 flex-fill border">支払い金額</div>
+          </div>
+          <div class="d-flex">
+            <div class="p-2 border w-25">預かり金額</div>
+            <div class="p-2 flex-fill border">差引残高</div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex">
+        <div class="d-flex align-content-center flex-wrap border bg-light bg-gradient" style="width:2em;">
+          <div class="p-2">1</div>
+        </div>
+        <div class="flex-fill">
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+        </div>
+        <div class="flex-fill">
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+          <div class="d-flex">
+            <div class="p-2 border w-25">　</div>
+            <div class="p-2 flex-fill border"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
