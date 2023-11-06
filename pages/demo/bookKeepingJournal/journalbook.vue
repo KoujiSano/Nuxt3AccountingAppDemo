008de49499373036e09ba7bafe3dd1c22fee50e2
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2023-11-06 06:03:43 +0000

    Refactor journalbook.vue to improve UI and UX

--- pages/demo/bookKeepingJournal/journalbook.vue
+++ pages/demo/bookKeepingJournal/journalbook.vue
@@ -1,29 +1,61 @@
 <template>
-    <p>Hello journalbook page</p>
-<div class="container">
+  <div class="container">
     <h1>仕訳帳</h1>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>会計単位</th>
-        <th>対象仕訳番号</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>対象日付範囲</td>
-        <td>年度</td>
-      </tr>
-      <tr>
-        <td>計上種類</td>
-        <td>付箋</td>
-      </tr>
-      <tr>
-        <td>入力種類区分</td>
-        <td>出力順</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
+    <table class="table">
+      <tbody>
+        <tr>
+          <th>会計単位</th>
+          <td>
+            <select class="form-select" aria-label="選択してください。">
+              <option value="1">本社</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <th>対象仕訳番号</th>
+          <td>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <th>対象日付範囲</th>
+          <td>
+            <select class="form-select" aria-label="選択してください。">
+              <option value="1">2023</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <th>年度</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>計上種類</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>付箋</th>
+        </tr>
+        <tr>
+          <th>入力種類区分</th>
+          <td></td>
+        </tr>
+        <tr>
+          <th>出力順</th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
