5dabdd6fda70d0077e9fd6320af93ba2ffc2ea8f
Kouta-awagatik <104189576+Kouta-awagatik@users.noreply.github.com>
2023-11-20 04:52:45 +0000

    commit

---
+++ pages/demo/CompaneySetting/setting.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <div class="form-group">
+        <label for="gender">性別を選択してください</label>
+        <div class="form-check">
+          <input type="radio" class="form-check-input" id="male" name="gender" value="male" v-model="selectedGender">
+          <label class="form-check-label" for="male">男性</label>
+        </div>
+        <div class="form-check">
+          <input type="radio" class="form-check-input" id="female" name="gender" value="female" v-model="selectedGender">
+          <label class="form-check-label" for="female">女性</label>
+        </div>
+        <div class="form-check">
+          <input type="radio" class="form-check-input" id="other" name="gender" value="other" v-model="selectedGender">
+          <label class="form-check-label" for="other">その他</label>
+        </div>
+      </div>
+  
+      <p>選択された性別: {{ selectedGender }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedGender: null
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  /* 必要に応じてスタイルを追加してください */
+  </style>
