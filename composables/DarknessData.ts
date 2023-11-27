3207e9c6bcaa368bfb9951ce3057a59a445321a8
Iris-Fla <irith.fla@gmail.com>
2023-11-27 16:31:43 +0900

    🎵ダークネスデータ修正

--- composables/DarknessData.ts
+++ composables/DarknessData.ts
@@ -1,6 +1,17 @@
 export const yeardata = () => {
-    return useState<number>('y', () => 1341)
+    return useState<number>('y', () => 3614)
   }
 export const kubundata = () => {
-    return useState<string>('k', () => "なーん..にゃん")
+    return useState<string>('k', () => "にゃにゃ～")
   }
+
+export const userdata = () => {
+    return useState('User', () => shallowRef({
+        Fiscal_year_end: "第壱刻",
+        CompanyName: "刹那月光",
+        Address: "紅に染まりし森林の夢",
+        Address2: "晴嵐-烈火街-",
+        Kubun: "なーん..にゃん",
+        Year: "1341",
+    }))
+}
