53cbbda4c3d9c500ca790fefef11f6cd1e43234f
Kouta-awagatik <104189576+Kouta-awagatik@users.noreply.github.com>
2023-11-27 04:47:33 +0000

    memo

--- pages/demo/CompaneySetting/setting.vue
+++ pages/demo/CompaneySetting/setting.vue
@@ -13,6 +13,14 @@
                 会計年度は期首日の年となります。
                 期中開始する場合も、期首日を求めて入力してください。
             </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                <label class="form-check-label" for="inlineCheckbox1">1</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                <label class="form-check-label" for="inlineCheckbox2">2</label>
+            </div>
         </div>
     </div>
     <div>
