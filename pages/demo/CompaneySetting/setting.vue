285fbdf60a6947bc142ac76e396ce5b19e2c6cd5
Kouta-awagatik <104189576+Kouta-awagatik@users.noreply.github.com>
2023-11-27 05:38:33 +0000

    memo

--- pages/demo/CompaneySetting/setting.vue
+++ pages/demo/CompaneySetting/setting.vue
@@ -1,59 +1,268 @@
 <template>
-    <div class="card mb-4" style="max-width: auto;">
-        <div class="card text-center">
-            <div class="card-header bg-info">
-                会計期間
-            </div>
-            <table class="table table-bordered">
-                <tr>
-                    <th>旗首日</th>
-                    <td>
-                        <input>
-                    </td>
-                </tr>
-            </table>
-            <div id="passwordHelpBlock" class="form-text">
-                <h5>* 会計年度 2023年<br>
-                    * 会計期間 2023年4月1日 から 2024年3月31日</h5>
-                期首日を元に1年後の日付が期末日として自動計算されます。
-                会計年度は期首日の年となります。
-                期中開始する場合も、期首日を求めて入力してください。
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                <label class="form-check-label" for="inlineCheckbox1">リセットしない</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                <label class="form-check-label" for="inlineCheckbox2">年毎にリセット</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                <label class="form-check-label" for="inlineCheckbox2">月毎にリセット</label>
-            </div>
-            tr>
-                    <th>決算期</th>
-                    <td>
-                        <input>
-                    </td>
-        </div>
+    <div class="container">
+      <h1>仕訳帳</h1>
+      <table class="table">
+        <tbody>
+          <tr>
+            <th>会計単位</th>
+            <td>
+              <select class="form-select" aria-label="選択してください。">
+                <option value="1">本社</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <th>対象仕訳番号</th>
+            <td>
+              <div class="mb-3">
+                <form class="form-floating">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="floatingInputValue"
+                    placeholder="仕訳番号"
+                    value="0000000"
+                  />
+                  <label for="floatingInputValue">仕訳番号</label>
+                </form>
+              </div>
+              <p>から</p>
+              <div>
+                <form class="form-floating">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="floatingInputValue"
+                    placeholder="仕訳番号"
+                    value="9999999"
+                  />
+                  <label for="floatingInputValue">仕訳番号</label>
+                </form>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>対象日付範囲</th>
+            <td>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="targetDateRange"
+                  id="targetDateRange1"
+                  checked
+                />
+                <label class="form-check-label" for="targetDateRange1">
+                  仕訳日付
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="targetDateRange"
+                  id="targetDateRange2"
+                />
+                <label class="form-check-label" for="targetDateRange2">
+                  入力日付
+                </label>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>年度</th>
+            <td>
+              <div class="mb-3">
+                <select class="form-select" aria-label="選択してください。">
+                  <option value="1">2023</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <select
+                  class="form-select"
+                  aria-label="選択してください。"
+                  multiple
+                >
+                  <option value="1">1月</option>
+                  <option value="2">2月</option>
+                  <option value="3">3月</option>
+                  <option value="4">4月</option>
+                  <option value="5">5月</option>
+                  <option value="6">6月</option>
+                  <option value="7">7月</option>
+                  <option value="8">8月</option>
+                  <option value="9">9月</option>
+                  <option value="10">10月</option>
+                  <option value="11">11月</option>
+                  <option value="12">12月</option>
+                </select>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>計上種類</th>
+            <td>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="reportingType"
+                  id="reportingType1"
+                  checked
+                />
+                <label class="form-check-label" for="reportingType1">
+                  全件
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="reportingType"
+                  id="reportingType2"
+                />
+                <label class="form-check-label" for="reportingType2">
+                  月次仕訳
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="reportingType"
+                  id="reportingType3"
+                />
+                <label class="form-check-label" for="reportingType3">
+                  再振替・決算仕訳
+                </label>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>付箋</th>
+            <td>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="tag"
+                  id="tag1"
+                  checked
+                />
+                <label class="form-check-label" for="tag1">
+                  全件
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="tag"
+                  id="tag2"
+                />
+                <label class="form-check-label" for="tag2">
+                  付箋あり
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="tag"
+                  id="tag3"
+                />
+                <label class="form-check-label" for="tag3">
+                  ファイル添付あり
+                </label>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>入力種類区分</th>
+            <td>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inputTypeClassification"
+                  id="inputTypeClassification1"
+                  checked
+                />
+                <label class="form-check-label" for="inputTypeClassification1">
+                  全件
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inputTypeClassification"
+                  id="inputTypeClassification2"
+                />
+                <label class="form-check-label" for="inputTypeClassification2">
+                  通常入力
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inputTypeClassification"
+                  id="inputTypeClassification3"
+                />
+                <label class="form-check-label" for="inputTypeClassification3">
+                  データ読み込み
+                </label>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th>出力順</th>
+            <td>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="outputOrder"
+                  id="outputOrder1"
+                  checked
+                />
+                <label class="form-check-label" for="outputOrder1">
+                  仕訳日付
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="outputOrder"
+                  id="outputOrder2"
+                />
+                <label class="form-check-label" for="outputOrder2">
+                  入力日付
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="outputOrder"
+                  id="outputOrder3"
+                />
+                <label class="form-check-label" for="outputOrder3">
+                  仕訳番号
+                </label>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <a href="/demo/bookKeepingJournal/search#search" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">表示</a>
+        <!-- <button class="btn btn-primary me-md-2" type="button">表示</button> -->
+      </div>
     </div>
-    <div>
-        <ul>
-            <p>決算報告書画面で決算書に表示する会計期間を変更することができます。</p>
-            <p>決算期は法人用決算報告書に出力されます。</p>
-            <p>仕訳番号のリセットは当年度以降の仕訳が存在している場合、変更することはできません。</p>
-        </ul>
-    </div>
-    <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-        <label class="form-check-label" for="inlineCheckbox1">1</label>
-    </div>
-    <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-        <label class="form-check-label" for="inlineCheckbox2">2</label>
-    </div>
-    <br>
-    <input type="checkbox" class="btn-check" id="btn-check" autocomplete="off">
-    <label class="btn btn-primary" for="btn-check">確認</label>
-</template>
+  </template>
+  
