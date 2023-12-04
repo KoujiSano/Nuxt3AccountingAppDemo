46b26d787228f89a661cb5ab44b2322cae372036
Kouta-awagatik <104189576+Kouta-awagatik@users.noreply.github.com>
2023-12-04 05:13:17 +0000

    mm

---
+++ components/Data Migrate/user.vue
@@ -0,0 +1,17 @@
+<template>
+    <Card
+    text-color="dark"
+    background-color="info"
+    margin="b-3"
+    style="max-width: 18rem"
+  >
+    <CardHeader>Header</CardHeader>
+    <CardBody>
+      <CardTitle>Info Card title</CardTitle>
+      <CardText>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </CardText>
+    </CardBody>
+  </Card>
+</template>
