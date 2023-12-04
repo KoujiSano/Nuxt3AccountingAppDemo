a93a45d9a9636e22ea9aa35e225e7d8f6164529e
sumna7 <104078088+syuu1230@users.noreply.github.com>
2023-12-04 14:56:42 +0900

    GeneralLedger

---
+++ pages/demo/general-ledger/aiueo.vue
@@ -0,0 +1,44 @@
+<template>
+    <card>
+    <BFormCheck inline>
+      <BFormCheckInput value="option1" />
+      <BFormCheckLabel>補助科目毎に出力</BFormCheckLabel>
+    </BFormCheck>
+    <BFormCheck inline>
+      <BFormCheckInput value="option2" />
+      <BFormCheckLabel>月毎改ページ</BFormCheckLabel>
+    </BFormCheck>
+    <BFormCheck inline>
+      <BFormCheckInput
+        value="option3"
+        disabled
+      />
+      <BFormCheckLabel>3 (disabled)</BFormCheckLabel>
+    </BFormCheck>
+    <BFormCheck inline>
+      <BFormCheckInput
+        type="radio"
+        name="inlineRadioOptions"
+        value="option1"
+      />
+      <BFormCheckLabel>1</BFormCheckLabel>
+    </BFormCheck>
+    <BFormCheck inline>
+      <BFormCheckInput
+        type="radio"
+        name="inlineRadioOptions"
+        value="option2"
+      />
+      <BFormCheckLabel>2</BFormCheckLabel>
+    </BFormCheck>
+    <BFormCheck inline>
+      <BFormCheckInput
+        type="radio"
+        name="inlineRadioOptions"
+        value="option3"
+        disabled
+      />
+      <BFormCheckLabel>3 (disabled)</BFormCheckLabel>
+    </BFormCheck>
+</card>
+  </template>
