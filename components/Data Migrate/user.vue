faf12313b802e1434761dea7c903c2c8eb17466a
Kouta-awagatik <104189576+Kouta-awagatik@users.noreply.github.com>
2023-12-04 05:15:49 +0000

    mm

--- components/Data Migrate/user.vue
+++ components/Data Migrate/user.vue
@@ -5,13 +5,12 @@
     margin="b-3"
     style="max-width: 18rem"
   >
-    <CardHeader>Header</CardHeader>
-    <CardBody>
-      <CardTitle>Info Card title</CardTitle>
-      <CardText>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </CardText>
+    <CardHeader>スイート会計シリーズへ移行</CardHeader>
+    <CardBody>     
+         <CardText>
+        円簿会計から株式会社フォーラムエイトが提供する「スイート会計シリーズ」へデータを移行することができます。
+        発行した移行コードはスイート会計シリーズにデータ移行するために必要になります。
+        </CardText>
     </CardBody>
   </Card>
 </template>
