9144b1ddfae26f8c19858f56a1359785624dbf57
Iris-Fla <irith.fla@gmail.com>
2023-12-11 13:53:27 +0900

    [Fix]💛

--- composables/UserData.ts
+++ composables/UserData.ts
@@ -1,8 +1,9 @@
 export const PublicUser = () => {
-    return useState('User', () => shallowRef({
+    return useState('User', () => ({
         Name: "ブラック黒之介",
         CompanyName: "漆黒かんぱに～",
         Address: "紅に染まりし森林の夢",
         Address2: "晴嵐-烈火街-",
+        KessanKi: "幽玄の刻"
     }))
 }
