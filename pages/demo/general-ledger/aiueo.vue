69420a48dc8d462276a685a606c37d94f04d7085
shu <KTC2243030009@edu.kyoto-tech.ac.jp>
2023-12-11 13:20:21 +0900

    generalLedger

--- pages/demo/general-ledger/aiueo.vue
+++ pages/demo/general-ledger/aiueo.vue
@@ -1,12 +1,11 @@
 <template>
-    <card>
     <BFormCheck inline>
       <BFormCheckInput value="option1" />
-      <BFormCheckLabel>補助科目毎に出力</BFormCheckLabel>
+      <BFormCheckLabel>1asdfasdfasdfasdfasdf</BFormCheckLabel>
     </BFormCheck>
     <BFormCheck inline>
       <BFormCheckInput value="option2" />
-      <BFormCheckLabel>月毎改ページ</BFormCheckLabel>
+      <BFormCheckLabel>2</BFormCheckLabel>
     </BFormCheck>
     <BFormCheck inline>
       <BFormCheckInput
@@ -40,5 +39,4 @@
       />
       <BFormCheckLabel>3 (disabled)</BFormCheckLabel>
     </BFormCheck>
-</card>
   </template>
