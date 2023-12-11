5aa1aa68f8554859c0772e8f909d1e67042ed8f4
Siguretuki <101462925+Siguretuki@users.noreply.github.com>
2023-12-11 06:01:40 +0000

    BasicInformationコンポーネントファイル作成

---
+++ components/BasicInformation/ProfitAndLoss.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-table bordered>
+        <b-thead>
+            <b-tr theme="success" text-alignment="center" text-wrap="nowrap">
+                <b-th scope="col" col="xs-2 sm-1">
+                
+                </b-th>
+                <b-th scope="col" col="xs-2 sm-1">
+                コード
+                </b-th>
+                <b-th scope="col" col="xs-6 sm-3" >
+                名称
+                </b-th>
+                <b-th scope="col" col="sm-3">
+                カナ
+                </b-th>
+                <b-th scope="col" col="sm-1" >
+                ローマ字
+                </b-th>
+                <b-th scope="col" col="xs-1 sm-1">
+                賃借
+                </b-th>
+                <b-th scope="col" col="xs-1 sm-1">
+                科目種類
+                </b-th>
+                <b-th scope="col" col="xs-1 sm-1">
+                消費税
+                </b-th>
+            </b-tr>
+        </b-thead>
+        <b-tbody>
+            <b-tr>
+                <b-td scope="row" text-alignment="center">
+                    <b-a href="#">
+                        変更
+                    </b-a>
+                </b-td>
+                <b-td text-alignment="left">Cell</b-td>
+                <b-td text-alignment="left">Cell</b-td>
+                <b-td text-alignment="left">Cell</b-td>
+                <b-td text-alignment="left">Cell</b-td>
+                <b-td text-alignment="center">Cell</b-td>
+                <b-td text-alignment="center">Cell</b-td>
+                <b-td text-alignment="center">Cell</b-td>
+            </b-tr>
+        </b-tbody>
+    </b-table>
+</template>
