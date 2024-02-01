3766a8d5e07b640eb415921dc49d5faac16dd6a7
Iris-Fla <irith.fla@gmail.com>
2024-02-02 01:35:20 +0900

    OrleansData 読み込みサンプル

--- pages/demo/financial-statement/details.vue
+++ pages/demo/financial-statement/details.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+const url = "http://localhost:5066/Accounting/AccountItem";
+
+const headers = new Headers({
+  Authorization: `Darkness`,
+});
+const res = await fetch(url, {
+  method: "GET",
+  headers: headers,
+});
+const resData = await res.json();
+</script>
 <template>
     <h1>Financial Statement Details</h1>
+    <h2>{{ resData[1] }}</h2>
 </template>
