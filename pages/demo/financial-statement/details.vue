32176deba03502c0aeec71ec966303623c2621d7
hiiragi1230 <104078088+hiiragi1230@users.noreply.github.com>
2024-02-08 11:07:21 +0900

    仮完成

--- pages/demo/financial-statement/details.vue
+++ pages/demo/financial-statement/details.vue
@@ -5,5 +5,10 @@ const { data:response } = await useFetch('/api/user/useOrleans?url=AccountItem',
 </script>
 <template>
     <h1>Financial Statement Details</h1>
-    <h2>{{ response[0] }}</h2>
+    <h2>{{ response[1] }}</h2>
+    <ul>
+        <li v-for="res in response">
+        {{ res }}
+        </li>
+    </ul>
 </template>
